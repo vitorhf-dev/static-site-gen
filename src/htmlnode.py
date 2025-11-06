99006dc29cd177ec3fe7d59804e7099e760b66bf
vitorhf-dev <vitorhfranca04@gmail.com>
2025-11-06 18:08:16 -0300

    Made a ParentNode class to handle the nesting of HTML nodes. Any HTML node that's not a leaf node (has children), is a parent node.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -30,3 +30,21 @@ class LeafNode(HTMLNode):
                 return f"{self.value}"
         else:
             return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        if tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if children is None:
+            raise ValueError("ParentNode must have children")
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if self.children is None:
+            raise ValueError("ParentNode must have children")
+        inner = "".join(child.to_html() for child in self.children)
+        return f"<{self.tag}{self.props_to_html()}>{inner}</{self.tag}>"
+        
+

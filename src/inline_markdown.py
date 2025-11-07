e4804f804a5a73a8b1748c551c05ca1bbd89783c
vitorhf-dev <vitorhfranca04@gmail.com>
2025-11-07 12:51:47 -0300

    Made 2 functions to extract the links and images in the Markdown file using regex

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,5 +1,5 @@
 from textnode import TextNode, TextType
-
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -20,3 +20,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+def extract_markdown_images(text: str):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    return re.findall(pattern, text)
+
+def extract_markdown_links(text: str):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    return re.findall(pattern, text)
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,4 +1,5 @@
 import unittest
+from inline_markdown import extract_markdown_images, extract_markdown_links
 from inline_markdown import (
     split_nodes_delimiter,
 )
@@ -85,6 +86,83 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+
+
+class TestInlineMarkdown(unittest.TestCase):
+    def test_single_image(self):
+        text = "Look ![alt](https://img.com/a.png)"
+        self.assertEqual(
+            extract_markdown_images(text),
+            [("alt", "https://img.com/a.png")]
+        )
+
+    def test_single_link(self):
+        text = "Go to [site](https://example.com)"
+        self.assertEqual(
+            extract_markdown_links(text),
+            [("site", "https://example.com")]
+        )
+
+    def test_multiple_images(self):
+        text = "![a](u1) and ![b](u2)"
+        self.assertEqual(
+            extract_markdown_images(text),
+            [("a", "u1"), ("b", "u2")]
+        )
+
+    def test_multiple_links(self):
+        text = "[x](u1) + [y](u2)"
+        self.assertEqual(
+            extract_markdown_links(text),
+            [("x", "u1"), ("y", "u2")]
+        )
+
+    def test_mixed_images_and_links(self):
+        text = "![pic](u) and [txt](v)"
+        self.assertEqual(
+            extract_markdown_images(text),
+            [("pic", "u")]
+        )
+        self.assertEqual(
+            extract_markdown_links(text),
+            [("txt", "v")]
+        )
+
+    def test_no_matches(self):
+        text = "Nothing here"
+        self.assertEqual(extract_markdown_images(text), [])
+        self.assertEqual(extract_markdown_links(text), [])
+
+    def test_empty_alt_and_text(self):
+        text = "![](img.png) and [](url)"
+        self.assertEqual(
+            extract_markdown_images(text),
+            [("", "img.png")]
+        )
+        self.assertEqual(
+            extract_markdown_links(text),
+            [("", "url")]
+        )
+
+    def test_negative_lookbehind_blocks_images_in_link_regex(self):
+        text = "![img](u) and [notimg](v)"
+        self.assertEqual(
+            extract_markdown_links(text),
+            [("notimg", "v")]
+        )
+
+    def test_exclamation_not_followed_by_bracket(self):
+        text = "Bang! [ok](u)"
+        self.assertEqual(
+            extract_markdown_links(text),
+            [("ok", "u")]
+        )
+
+    def test_double_bang_image(self):
+        text = "!![img](u)"
+        # Image should still match once; link should not match the image
+        self.assertEqual(extract_markdown_images(text), [("img", "u")])
+        self.assertEqual(extract_markdown_links(text), [])
 
 
 if __name__ == "__main__":
